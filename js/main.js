822066275bfbda74b4d2f211c19553983065b0ec
Vyacheslav <88817890+SlavikKlimov1988@users.noreply.github.com>
2021-08-26 22:06:43 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,55 @@
+// Переменные
+
+var num = 1;
+let num = 1;
+const num = 1;
+
+let a ;
+a = 'vova';
+console.log(a);
+
+// преобразование типов(явное приведение и не явное)
+
+let num = String(10); // "10"
+
+let vel = (40).toString; // "40"
+velue = String([1, 2, 3]); // '"1,2,3"
+let res = 4 + ''; // "4"
+velue = true + 10; // 11
+velue = Number("10"); // 10
+velue = parseInt("12 number"); // 12
+value = Boolean("folse"); // true
+value = Boolean( 0 ); //undefined, - folse
+
+
+//Numbers
+let num1 = 10 ;
+let num2 = 10;
+let result = num1 + num2 // * / - % 
+velue = 4 % 2; // 0
+vel++ // +1
+++vel // 0(+1)
+vel--
+--vel
+
+res = 0.3+0.4;
+resul = res.toFixed(1);
+
+// Math (object)
+
+velue = Math;
+console.log(velue);
+a = Math.random();
+b = Math.round(2.777)
+c = Math.ceil(2.666)
+d = Math.floor(2.888)
+e = Math.max(1,2,3,4,5,6,78,) // min
+
+v = Math.random() * 10; // 1.11111111
+
+const arr = ["bleck", "green", 'red']
+ 
+res = Math.floor(Math.random()*arr.length);
+
+arr[res]
+
